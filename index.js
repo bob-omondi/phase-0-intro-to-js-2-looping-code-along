33464efc8987143e5b5afad1a0470f317ecfee22
+ <+>
2022-08-10 18:04:44 +0300

    complete assigned

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+// Code your solutions in this fileclear
+const names = []
+ function writeCards(names, birthday) {
+  for (let i = 0; i < names.length; i++) {
+console.log(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+    debugger;
+  }
+
+  return writeCards;
+}
+
+writeCards(names, birthday) ; 
+
+var arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
+for (var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+
+  
+    function countDown(){
+      let countDown = 10;
+      while (countDown < 10) {
+       alert(countDown--);
+}
+      }
+    
+
+  
