e92a2320b5b5f3cafa1740a3476dc3efc90b2187
+ <+>
2022-08-27 15:30:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 // Code your solutions in this fileclear
-const names = []
- function writeCards(names, birthday) {
-  for (let i = 0; i < names.length; i++) {
-console.log(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
-    debugger;
+const writeCards = (array,surprise)=>{
+  let emptyArray =[]
+
+  for (item of array){
+       emptyArray.push( `Thank you, ${item}, for the wonderful ${surprise} gift!`) 
   }
+  return emptyArray
 
-  return writeCards;
 }
 
-writeCards(names, birthday) ; 
-
-var arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
- 
-for (var i = arr.length - 1; i >= 0; i--) {
-    console.log(arr[i]);
+const countDown = (number)=>{
+  for (let i = number;i>=0;i--){
+      console.log(i)
+  }
 }
 
-  
-    function countDown(){
-      let countDown = 10;
-      while (countDown < 10) {
-       alert(countDown--);
-}
-      }
-    
-
-  
